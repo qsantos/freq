3b1ea9b8b22ea9fbf8df076ae550f08e864f436f
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-01-08 23:21:05 +0100

    Added usage()

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+
+static void usage(int argc, char** argv)
+{
+	(void) argc;
+
+	fprintf(stderr,
+		"Usage: %s blocksize file\n"
+		"\n"
+		"PARAMS:\n"
+		" blocksize  the byte-length of blocks\n"
+		" file       the name of the file to be analyzed\n"
+		,
+		argv[0]
+	);
+}
+
 int main(int argc, char** argv)
 {
 	(void) argc;
