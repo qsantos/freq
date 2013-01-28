cd068864f3f92b7ff11b3bcececebbf7c0ee3edc
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-01-28 23:08:25 +0100

    Moved letter frequencies to separate file

--- alpha.c
+++ alpha.c
@@ -2,13 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static const float ref[26] =
-{
-	0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094,
-	0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929,
-	0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
-	0.01974, 0.00074,
-};
+#include "freq.h" // letter frequencies
 
 static char* readFile(FILE* f)
 {
@@ -58,7 +52,7 @@ static char bestShift(float freq[26])
 		float score = 0;
 		for (int j = 0; j < 26; j++)
 		{
-			float diff = freq[(j+i)%26] - ref[j];
+			float diff = freq[(j+i)%26] - english[j];
 			score += diff*diff;
 		}
 		if (score < best_score)
